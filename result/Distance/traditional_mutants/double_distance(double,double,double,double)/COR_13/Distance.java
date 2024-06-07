3b7476d46e270ac3679e4b5019931ea3fd2a68cd
Chris Murphy <cdmurphy@seas.upenn.edu>
2024-06-07 13:38:55 -0400

    added mutations

---
+++ result/Distance/traditional_mutants/double_distance(double,double,double,double)/COR_13/Distance.java
@@ -0,0 +1,35 @@
+
+public class Distance {
+	
+	/*
+	 * Method to calculate the distance between (x1, y1) and (x2, y2) using
+	 * great-circle distance.
+	 */
+	public static double distance(double x1, double y1, double x2, double y2) {
+		
+		if (x1 < -90 || x1 > 90) {
+			throw new IllegalArgumentException("x1 out of range");
+		}
+else if (y1 > -180 || y1 > 180) { 
+			throw new IllegalArgumentException("y1 out of range");
+		}
+		else if (x2 < -90 || x2 > 90) {
+			throw new IllegalArgumentException("x2 out of range");
+		}
+		else if (y2 < -180 || y2 > 180) {
+			throw new IllegalArgumentException("y2 out of range");
+		}
+		
+        double degree_x1 = Math.toRadians(x1);
+        double degree_y1 = Math.toRadians(y1);
+        double degree_x2 = Math.toRadians(x2);
+        double degree_y2 = Math.toRadians(y2);
+        
+        double distance = 3986 * (Math.acos(Math.sin(degree_x1) * Math.sin(degree_x2)
+        + Math.cos(degree_x1) * Math.cos(degree_x2) * Math.cos(degree_y1 - degree_y2)));
+		
+		return distance;
+	}
+
+}
+// TYPE_ID:13:double_distance(double,double,double,double):else if (y1 < -180 || y1 > 180) { => else if (y1 > -180 || y1 > 180) {
